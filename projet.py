d81accd8d10595dcda20e05390614a282e515a05
L-mrt <l-mrt@example.com>
2026-01-16 16:16:06 +0100

    Fix code to use frequency data from france_2019_05.csv and correct Pre/Pbid calculation

--- projet.py
+++ projet.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Q1
 # Import data
-df = pd.read_csv("/content/france_2019_05.csv")
-df.head()
+df = pd.read_csv("data/france_2019_05.csv")
+print(df.head())
 
 # Construction of values Pre/Pbid
-df_FR["Frequency"] = df_FR["Frequency"]*10**(-3)
-df["Pre/Pbid"] = 5*(df_FR["Frequency"])
-df.head()
+df["Frequency"] = df["Frequency"]*10**(-3)
+df["Pre/Pbid"] = 5*(df["Frequency"])
+print(df[["Frequency", "Pre/Pbid"]].head())
 
 # Plot of the distribution Pre/Pbid
 df["Pre/Pbid"].hist(bins=200)
@@ -21,7 +22,7 @@ df["Pre/Pbid"].describe()
 
 # Q2 :The regulating power tries to balance the frequency between -0.2 Hz and 0.2Hz
 
-#Q3
+# Q3
 E_batt = 46 # 𝐸𝑏𝑎𝑡𝑡 the useful energy capacity of the battery in kWh
 P_max = 7 # Power of bindirectional transfert of the battery in kW
 P_bid = P_max/1.1 #
